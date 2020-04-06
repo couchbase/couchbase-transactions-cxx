0d63af2ef75b2075745ddde7017e9ec113817699
Sergey Avseyev <sergey.avseyev@gmail.com>
2020-04-06 23:07:00 +0300

    add mode client exception classes

--- include/couchbase/client/exceptions.hxx
+++ include/couchbase/client/exceptions.hxx
@@ -4,13 +4,39 @@
 
 namespace couchbase
 {
-class not_found_error : public std::runtime_error
+class document_not_found_error : public std::runtime_error
 {
   public:
-    explicit not_found_error(const std::string& what)
+    explicit document_not_found_error(const std::string& what)
       : std::runtime_error(what)
     {
     }
 };
 
+class document_exists_error : public std::runtime_error
+{
+  public:
+    explicit document_exists_error(const std::string& what)
+      : std::runtime_error(what)
+    {
+    }
+};
+
+class path_not_found_error : public std::runtime_error
+{
+  public:
+    explicit path_not_found_error(const std::string& what)
+      : std::runtime_error(what)
+    {
+    }
+};
+
+class cas_mismatch_error : public std::runtime_error
+{
+  public:
+    explicit cas_mismatch_error(const std::string& what)
+      : std::runtime_error(what)
+    {
+    }
+};
 } // namespace couchbase
