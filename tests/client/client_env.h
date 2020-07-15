88a267104c84fee35e8fc7b4f157d6226aea9d35
David Kelly <davidmichaelkelly@gmail.com>
2020-07-15 08:54:48 -0600

    TXNCXX-11 Implement API
    Motivation
    ==========
    All we want are some initial tests, to consider this done.  These are
    very simple client tests only.  More to come, but this is a start.
    
    Modification
    ============
    A little CMake work to pull in gtest, and a set of very simple tests
    of cluster, bucket and collection.  Right now we require a running
    server but probably more appropriate to use the mock, later.
    
    Results
    =======
    A single executable (client_tests) which one can run from the build
    directory.  Ideally I'd have a make target for this, but there's a bit
    of voodoo necessary to get the gtest_add_test stuff working.  Will definitely
    do that soon.
    
    Change-Id: Ia9d71d13cd2a8308cd031ae3cc6dac89c82c5d69
    Reviewed-on: http://review.couchbase.org/c/couchbase-transactions-cxx/+/132586
    Reviewed-by: Sergey Avseyev
    Tested-by: David Kelly <davidmichaelkelly@gmail.com>

---
+++ tests/client/client_env.h
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <cstdlib>
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+#include "couchbase/client/cluster.hxx"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
+// hack, until I get gtest working a bit better and can execute
+// tests through make with proper working directory.
+#define CONFIG_FILE_NAME "../tests/config.json"
+#define ENV_CONNECTION_STRING "TXN_CONNECTION_STRING"
+
+class ClientTestEnvironment : public ::testing::Environment {
+  public:
+    void SetUp() override {
+        // for tests, really chatty logs may be useful.
+        spdlog::set_level(spdlog::level::trace);
+        get_cluster();
+    }
+
+    static const nlohmann::json& get_conf() {
+        // read config.json
+        static nlohmann::json conf;
+        if (conf.empty()) {
+            spdlog::info("reading config file {}", CONFIG_FILE_NAME);
+            std::ifstream in(CONFIG_FILE_NAME, std::ifstream::in);
+            conf = nlohmann::json::parse(in);
+            char* override_conn_str = std::getenv(ENV_CONNECTION_STRING);
+            if (override_conn_str) {
+                spdlog::trace("overriding connection string - '{}'", override_conn_str);
+                conf["connection_string"] = override_conn_str;
+            }
+        }
+        return conf;
+
+    }
+
+    static std::shared_ptr<couchbase::cluster> get_cluster() {
+        auto conf = get_conf();
+        static auto cluster = std::make_shared<couchbase::cluster>(conf["connection_string"], conf["username"], conf["password"]);
+        return cluster;
+    }
+
+    static std::string get_uuid() {
+        static auto generator = boost::uuids::random_generator();
+        return boost::uuids::to_string(generator());
+    }
+
+    static std::shared_ptr<couchbase::collection> get_default_collection(const std::string& bucket_name) {
+        auto c = get_cluster();
+        auto b = c->bucket(bucket_name);
+        return b->default_collection();
+    }
+};
